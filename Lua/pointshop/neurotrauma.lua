de0c38b9e31547dbc15fba3df11ba92b9f6eec0a
EvilFactory <evilfactory@users.noreply.github.com>
2022-10-29 18:04:53 -0300

    Add medcrate

--- Lua/pointshop/neurotrauma.lua
+++ Lua/pointshop/neurotrauma.lua
@@ -3,7 +3,12 @@ local category = {}
 category.Name = "Neurotrauma"
 
 category.Products = {
-    
+    {
+        Name = "Medical Starter Crate",
+        Price = 150,
+        Limit = 3,
+        Items = {"medstartercrate"}
+    },
 }
 
 return category
